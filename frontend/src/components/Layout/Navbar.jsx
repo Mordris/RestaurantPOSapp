6b1b5d192a0867d412eff9ab01fb92e509e05af7
Yunus Emre Gültepe <yunus.gultepe@ogr.sakarya.edu.tr>
2024-08-14 21:24:07 +0300

    Fixed index.html and logo

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -4,19 +4,25 @@ import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import TableViewIcon from "@mui/icons-material/TableView";
 import HistoryIcon from "@mui/icons-material/History";
+import Logo from "../../assets/appLogo.png"; // Adjust the path to your logo file
 
 const Navbar = () => (
   <AppBar position="static">
     <Toolbar>
+      <img
+        src={Logo}
+        alt="DineMaster POS Logo"
+        style={{
+          width: 40,
+          height: 40,
+          marginRight: 16,
+          filter: "invert(1) brightness(0) invert(1)",
+        }}
+      />
       <Typography variant="h6" sx={{ flexGrow: 1 }}>
         POS System
       </Typography>
-      <Button
-        color="inherit"
-        component={Link}
-        to="/"
-        startIcon={<HomeIcon />}
-      >
+      <Button color="inherit" component={Link} to="/" startIcon={<HomeIcon />}>
         Home
       </Button>
       <Button
